c16c5bd2128b518b3f7a0fdc85dc6562007bd124
Eric Do <beatyshot@gmail.com>
2022-11-13 00:59:38 +1000

    some works

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/category/getting-started"
           >
-            Getting Started ⏱️
+            Getting Started
           </Link>
         </div>
       </div>
