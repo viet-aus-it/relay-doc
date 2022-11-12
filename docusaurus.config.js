1f9d203104003545b73d6ad82d9f0b724d2cba4a
Eric Do <beatyshot@gmail.com>
2022-11-12 18:47:58 +1000

    fix link error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,7 +85,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro/intro',
+                to: '/docs/category/introduction',
               },
             ],
           },
